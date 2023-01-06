31b8c94d6c7a2f2967f823dd3db9704c800bb32d
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-06 11:08:56 +0100

    Color sphere using surface normal

--- main.cpp
+++ main.cpp
@@ -6,22 +6,30 @@
 
 using namespace std;
 
-bool hit_sphere(const Point3 &center, double radius, const Ray &ray) {
+double hit_sphere(const Point3 &center, double radius, const Ray &ray) {
     Vec3 origin_center = ray.origin() - center;
     auto a = dot(ray.direction(), ray.direction());
     auto b = 2.0 * dot(origin_center, ray.direction());
     auto c = dot(origin_center, origin_center) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return discriminant > 0;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 Color ray_color(const Ray &ray) {
-    if (hit_sphere(Point3(0, 0, -1), 0.5, ray)) {
-        return {1, 0, 0};
+    auto t = hit_sphere(Point3(0, 0, -1), 0.5, ray);
+
+    if (t > 0.0) {
+        Vec3 N = unit_vector(ray.at(t) - Vec3(0, 0, -1));
+        return 0.5 * Color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     Vec3 unit_direction = unit_vector(ray.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
 
