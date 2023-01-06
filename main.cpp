d9fb3d1f02a768a27e65d8e6fced22ebf4b1c297
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-06 10:47:56 +0100

    Begin to ray trace in empty scene

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include "Color.h"
+#include "Ray.h"
 #include "Vec3.h"
 
 #include <iostream>
 
 using namespace std;
 
+Color ray_color(const Ray &ray) {
+    Vec3 unit_direction = unit_vector(ray.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
+
 int main() {
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const auto image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = Point3(0, 0, 0);
+    auto horizontal = Vec3(viewport_width, 0, 0);
+    auto vertical = Vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focal_length);
 
     // Render
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -16,10 +34,16 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         cerr << "\rScanlines remaining: " << j << ' ' << flush;
         for (int i = 0; i < image_width; ++i) {
-            Color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+
+            Ray ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            Color pixel_color = ray_color(ray);
             write_color(cout, pixel_color);
         }
     }
+
+    cerr << "\nDone.\n";
 
     return 0;
 }
