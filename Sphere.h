474b769ac7e1cb7e4019ba1020d463b899bdb9e3
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-09 13:53:45 +0100

    Add material information to sphere

--- Sphere.h
+++ Sphere.h
@@ -1,6 +1,8 @@
 #ifndef RAY_TRACING_IN_CPP_SPHERE_H
 #define RAY_TRACING_IN_CPP_SPHERE_H
 
+#include <utility>
+
 #include "Hittable.h"
 #include "Vec3.h"
 
@@ -8,10 +10,12 @@ class Sphere : public Hittable {
 public:
     Point3 _center;
     double _radius = 0.0;
+    std::shared_ptr<Material> _material_ptr;
 
     Sphere() = default;
 
-    Sphere(Point3 center, double radius) : _center(center), _radius(radius) {};
+    Sphere(Point3 center, double radius, shared_ptr<Material> material) : _center(center), _radius(radius),
+                                                                          _material_ptr(std::move(material)) {};
 
     bool hit(const Ray &r, double t_min, double t_max, Hit_record &record) const override;
 };
@@ -40,6 +44,7 @@ bool Sphere::hit(const Ray &ray, double t_min, double t_max, Hit_record &record)
     record.point = ray.at(root);
     Vec3 outward_normal = (record.point - _center) / _radius;
     record.set_face_normal(ray, outward_normal);
+    record.material_ptr = _material_ptr;
 
     return true;
 }
