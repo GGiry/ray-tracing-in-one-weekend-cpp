f67fcb84b7ba6a1114e72a06562a7f9ade7d9a6a
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-10 13:45:00 +0100

    Add open/close shutter time to camera

--- Camera.h
+++ Camera.h
@@ -13,6 +13,8 @@ public:
     Vec3 v;
     Vec3 w;
     double lens_radius;
+    double shutter_open_time;
+    double shutter_close_time;
 
     Camera(
             Point3 look_from,
@@ -21,8 +23,10 @@ public:
             double vertical_field_of_view,    // Vertical field-of-view in degree
             double aspect_ratio,
             double aperture,
-            double focus_distance
-    ) : origin(look_from), lens_radius(aperture / 2) {
+            double focus_distance,
+            double open_time = 0,
+            double close_time = 0
+    ) : origin(look_from), lens_radius(aperture / 2), shutter_open_time(open_time), shutter_close_time(close_time) {
         auto theta = degrees_to_radians(vertical_field_of_view);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
@@ -40,7 +44,9 @@ public:
     [[nodiscard]] Ray get_ray(double s, double t) const {
         Vec3 rd = lens_radius * random_in_unit_disk();
         Vec3 offset = u * rd.x() + v * rd.y();
-        return {origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset};
+        return {origin + offset,
+                lower_left_corner + s * horizontal + t * vertical - origin - offset,
+                random_double(shutter_open_time, shutter_close_time)};
     }
 };
 
