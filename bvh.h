641fbcaacf0ab2edbbc5bdee58df9c305d5e979b
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-10 16:19:50 +0100

    Complete BVH node constructor

--- bvh.h
+++ bvh.h
@@ -31,7 +31,7 @@ bool BVH_node::bounding_box(double time0, double time1, AABB &output_box) const 
     return true;
 }
 
-bool Bounding_Volume_Hierarchy_node::hit(const Ray &ray, double t_min, double t_max, Hit_record &record) const {
+bool BVH_node::hit(const Ray &ray, double t_min, double t_max, Hit_record &record) const {
     if (!box.hit(ray, t_min, t_max)) { return false; }
 
     bool hit_left = left->hit(ray, t_min, t_max, record);
@@ -39,5 +39,43 @@ bool Bounding_Volume_Hierarchy_node::hit(const Ray &ray, double t_min, double t_
 
     return hit_left || hit_right;
 }
+
+BVH_node::Bounding_Volume_Hierarchy_node(const vector<shared_ptr<Hittable>> &src_objects,
+                                         size_t start, size_t end, double time0, double time1) {
+    auto objects = src_objects; // modifiable array of the source
+
+    int axis = random_int(0, 2);
+    auto comparator = (axis == 0) ? box_x_compare
+                                  : (axis == 1) ? box_y_compare
+                                                : box_z_compare;
+
+
+    if (size_t object_span = end - start; object_span == 1) {
+        left = right = objects[start];
+    } else if (object_span == 2) {
+        if (comparator(objects[start], objects[start + 1])) {
+            left = objects[start];
+            right = objects[start + 1];
+        } else {
+            left = objects[start + 1];
+            right = objects[start];
+        }
+    } else {
+        std::sort(objects.begin() + start, objects.begin() + end, comparator);
+        auto mid = start + object_span / 2;
+        left = std::make_shared<BVH_node>(objects, start, mid, time0, time1);
+        right = std::make_shared<BVH_node>(objects, mid, end, time0, time1);
+    }
+
+    AABB box_left;
+    AABB box_right;
+
+    if (!left->bounding_box(time0, time1, box_left) || !right->bounding_box(time0, time1, box_right)) {
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+    }
+
+    box = surrounding_box(box_left, box_right);
+}
+
 
 #endif //RAY_TRACING_IN_CPP_BVH_H
