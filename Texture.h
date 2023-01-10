81478497d24cd154931133c9bd60ec633f014939
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-10 17:29:38 +0100

    Create checker texture

--- Texture.h
+++ Texture.h
@@ -1,6 +1,8 @@
 #ifndef RAY_TRACING_IN_CPP_TEXTURE_H
 #define RAY_TRACING_IN_CPP_TEXTURE_H
 
+#include <utility>
+
 #include "util.h"
 
 class Texture {
@@ -26,5 +28,26 @@ private:
     Color color_value;
 };
 
+class Checker_texture : public Texture {
+public:
+    std::shared_ptr<Texture> even;
+    std::shared_ptr<Texture> odd;
+
+    Checker_texture() = default;
+
+    Checker_texture(std::shared_ptr<Texture> _even, std::shared_ptr<Texture> _odd) : even(std::move(_even)),
+                                                                                     odd(std::move(_odd)) {}
+
+    Checker_texture(Color _even, Color _odd) : even(std::make_shared<Solid_color>(_even)),
+                                               odd(std::make_shared<Solid_color>(_odd)) {}
+
+    Color value(double u, double v, const Point3 &point) const override {
+
+        if (auto sines = sin(10 * point.x()) * sin(10 * point.y()) * sin(10 * point.z()); sines < 0) {
+            return odd->value(u, v, point);
+        }
+        return even->value(u, v, point);
+    }
+};
 
 #endif //RAY_TRACING_IN_CPP_TEXTURE_H
