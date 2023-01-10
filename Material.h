ac0e32340eb29998e0b1ecb9746f804783b9ad47
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-10 09:56:30 +0100

    Auto-format file

--- Material.h
+++ Material.h
@@ -97,15 +97,14 @@ public:
 
         Vec3 unit_direction = unit_vector(r_in.direction());
         double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
-        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
 
         bool cannot_refract = refraction_ratio * sin_theta > 1.0;
         Vec3 direction;
 
         if (cannot_refract) {
             direction = reflect(unit_direction, rec.normal);
-        }
-        else {
+        } else {
             direction = refract(unit_direction, rec.normal, refraction_ratio);
         }
 
