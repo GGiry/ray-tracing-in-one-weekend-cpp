7593bf90a09d90b0e938b8fb3ba8aff73bc5b280
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-10 13:37:34 +0100

    Add time to Ray

--- Ray.h
+++ Ray.h
@@ -7,14 +7,18 @@ class Ray {
 public:
     Point3 _origin;
     Vec3 _direction;
+    double _time = 0.0;
 
     Ray() = default;
 
-    Ray(const Point3 &origin, const Vec3 &direction) : _origin(origin), _direction(direction) {}
+    Ray(const Point3 &origin, const Vec3 &direction, double time = 0.0) :
+            _origin(origin), _direction(direction), _time(time) {}
 
     [[nodiscard]] Point3 origin() const { return _origin; }
 
     [[nodiscard]] Vec3 direction() const { return _direction; }
+
+    [[nodiscard]] double time() const { return _time; }
 
     [[nodiscard]] Point3 at(double t) const {
         return _origin + t * _direction;
