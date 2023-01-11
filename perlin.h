568a9c79bea8961f42860b4e3dc5b7c6a7344184
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-11 09:52:44 +0100

    Use random vectors instead of floats

--- perlin.h
+++ perlin.h
@@ -6,9 +6,9 @@
 class Perlin {
 public:
     Perlin() {
-        random_floats = std::vector<double>(point_count);
+        random_vectors = std::vector<Vec3>(point_count);
         for (int i = 0; i < point_count; ++i) {
-            random_floats[i] = random_double();
+            random_vectors[i] = unit_vector(Vec3::random(-1, 1));
         }
 
         perm_x = perlin_generate_perm();
@@ -29,13 +29,13 @@ public:
         auto j = static_cast<int>(floor(point.y()));
         auto k = static_cast<int>(floor(point.z()));
 
-        vector<vector<vector<double>>> c{{{0, 0}, {0, 0}},
-                                         {{0, 0}, {0, 0}}};
+        vector<vector<vector<Vec3>>> c{{{Vec3(0, 0, 0), Vec3(0, 0, 0)}, {Vec3(0, 0, 0), Vec3(0, 0, 0)}},
+                                       {{Vec3(0, 0, 0), Vec3(0, 0, 0)}, {Vec3(0, 0, 0), Vec3(0, 0, 0)}}};
 
         for (int di = 0; di < 2; di++) {
             for (int dj = 0; dj < 2; dj++) {
                 for (int dk = 0; dk < 2; dk++) {
-                    c[di][dj][dk] = random_floats[
+                    c[di][dj][dk] = random_vectors[
                             perm_x[(i + di) & 255] ^
                             perm_y[(j + dj) & 255] ^
                             perm_z[(k + dk) & 255]
@@ -44,12 +44,12 @@ public:
             }
         }
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
     }
 
 private:
     static const int point_count = 256;
-    std::vector<double> random_floats;
+    std::vector<Vec3> random_vectors;
     std::vector<int> perm_x;
     std::vector<int> perm_y;
     std::vector<int> perm_z;
@@ -83,6 +83,25 @@ private:
                     accum += (i * u + (1 - i) * (1 - u)) *
                              (j * v + (1 - j) * (1 - v)) *
                              (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+
+        return accum;
+    }
+
+    static double perlin_interp(vector<vector<vector<Vec3>>> c, double u, double v, double w) {
+        auto uu = u * u * (3 - 2 * u);
+        auto vv = v * v * (3 - 2 * v);
+        auto ww = w * w * (3 - 2 * w);
+        auto accum = 0.0;
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++) {
+                    Vec3 weight_v(u - i, v - j, w - k);
+                    accum += (i * uu + (1 - i) * (1 - uu))
+                             * (j * vv + (1 - j) * (1 - vv))
+                             * (k * ww + (1 - k) * (1 - ww))
+                             * dot(c[i][j][k], weight_v);
+                }
 
         return accum;
     }
