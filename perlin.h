283a06b03d57d1b8a893e6622612e77700fc3c0b
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-11 09:18:25 +0100

    Smooth perlin noise

--- perlin.h
+++ perlin.h
@@ -17,11 +17,30 @@ public:
     }
 
     [[nodiscard]] double noise(const Point3 &point) const {
-        auto i = static_cast<int>(4 * point.x()) & 255;
-        auto j = static_cast<int>(4 * point.y()) & 255;
-        auto k = static_cast<int>(4 * point.z()) & 255;
+        auto u = point.x() - floor(point.x());
+        auto v = point.y() - floor(point.y());
+        auto w = point.z() - floor(point.z());
 
-        return random_floats[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        auto i = static_cast<int>(floor(point.x()));
+        auto j = static_cast<int>(floor(point.y()));
+        auto k = static_cast<int>(floor(point.z()));
+
+        vector<vector<vector<double>>> c{{{0, 0}, {0, 0}},
+                                         {{0, 0}, {0, 0}}};
+
+        for (int di = 0; di < 2; di++) {
+            for (int dj = 0; dj < 2; dj++) {
+                for (int dk = 0; dk < 2; dk++) {
+                    c[di][dj][dk] = random_floats[
+                            perm_x[(i + di) & 255] ^
+                            perm_y[(j + dj) & 255] ^
+                            perm_z[(k + dk) & 255]
+                    ];
+                }
+            }
+        }
+
+        return trilinear_interp(c, u, v, w);
     }
 
 private:
@@ -51,7 +70,18 @@ private:
             p[target] = tmp;
         }
     }
-};
 
+    static double trilinear_interp(vector<vector<vector<double>>> c, double u, double v, double w) {
+        auto accum = 0.0;
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++)
+                    accum += (i * u + (1 - i) * (1 - u)) *
+                             (j * v + (1 - j) * (1 - v)) *
+                             (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+
+        return accum;
+    }
+};
 
 #endif //RAY_TRACING_IN_CPP_PERLIN_H
