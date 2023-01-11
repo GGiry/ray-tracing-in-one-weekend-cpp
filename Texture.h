e22d4efcf7728d37f19ebbda63c5871261fc7e2b
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-11 10:30:13 +0100

    Add phase to perturbation

--- Texture.h
+++ Texture.h
@@ -62,7 +62,7 @@ public:
 
 
     [[nodiscard]] Color value(double u, double v, const Point3 &point) const override {
-        return Color(1, 1, 1) * noise.turbulence(scale * point);
+        return Color(1, 1, 1) * 0.5 * (1 + sin(scale * point.z() + 10 * noise.turbulence(scale * point)));
     }
 };
 
