1d3c789c545a20a676ea524fe3630091d52bd6ce
Geoffrey GIRY <geoffrey.giry@smile.fr>
2023-01-19 17:58:46 +0100

    Put common code for rotation in one class

--- Hittable.h
+++ Hittable.h
@@ -62,7 +62,7 @@ bool Translate::bounding_box(double time0, double time1, AABB &output_box) const
     return true;
 }
 
-class Rotate_y : public Hittable {
+class Rotate : public Hittable {
 public:
     shared_ptr<Hittable> object;
     double sin_theta;
@@ -70,7 +70,7 @@ public:
     bool hasbox;
     AABB bbox;
 
-    Rotate_y(const shared_ptr<Hittable> &_object, double angle);
+    Rotate(const shared_ptr<Hittable> &_object, double angle);
 
     bool hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const override;
 
@@ -79,15 +79,75 @@ public:
         return hasbox;
     }
 
+    void compute_AABB();
+
 private:
-    [[nodiscard]] inline Vec3 rotateY(const Vec3 &vec) const {
+    [[nodiscard]] virtual inline Vec3 rotate(const Vec3 &vec) const = 0;
+
+    [[nodiscard]] virtual inline Vec3 inverse_rotate(const Vec3 &vec) const = 0;
+};
+
+Rotate::Rotate(const shared_ptr<Hittable> &_object, double angle) : object(_object) {
+    auto radians = degrees_to_radians(angle);
+    sin_theta = sin(radians);
+    cos_theta = cos(radians);
+    hasbox = _object->bounding_box(0, 1, bbox);
+}
+
+bool Rotate::hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const {
+    auto origin = inverse_rotate(ray.origin());
+    auto direction = inverse_rotate(ray.direction());
+
+    Ray rotated_ray(origin, direction, ray.time());
+
+    if (!object->hit(rotated_ray, t_min, t_max, rec)) { return false; }
+
+    auto point = rotate(rec.point);
+    auto normal = rotate(rec.normal);
+
+    rec.point = point;
+    rec.set_face_normal(rotated_ray, normal);
+
+    return true;
+}
+
+void Rotate::compute_AABB() {
+    Point3 min(infinity, infinity, infinity);
+    Point3 max(-infinity, -infinity, -infinity);
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                auto x = i * bbox.max().x() + (1 - i) * bbox.min().x();
+                auto y = j * bbox.max().y() + (1 - j) * bbox.min().y();
+                auto z = k * bbox.max().z() + (1 - k) * bbox.min().z();
+
+                Vec3 tester = rotate(Vec3(x, y, z));
+                for (int c = 0; c < 3; ++c) {
+                    min[c] = fmin(min[c], tester[c]);
+                    max[c] = fmax(max[c], tester[c]);
+                }
+            }
+        }
+    }
+
+    bbox = AABB(min, max);
+}
+
+
+class Rotate_y : public Rotate {
+public:
+    Rotate_y(const shared_ptr<Hittable> &_object, double angle);
+
+private:
+    [[nodiscard]] inline Vec3 rotate(const Vec3 &vec) const override {
         double new_x = cos_theta * vec.x() + sin_theta * vec.z();
         double new_z = -sin_theta * vec.x() + cos_theta * vec.z();
 
         return {new_x, vec.y(), new_z};
     }
 
-    [[nodiscard]] inline Vec3 inverse_rotateY(const Vec3 &vec) const {
+    [[nodiscard]] inline Vec3 inverse_rotate(const Vec3 &vec) const override {
         double new_x = cos_theta * vec.x() - sin_theta * vec.z();
         double new_z = sin_theta * vec.x() + cos_theta * vec.z();
 
@@ -95,78 +155,26 @@ private:
     }
 };
 
-Rotate_y::Rotate_y(const shared_ptr<Hittable> &_object, double angle) : object(_object) {
-    auto radians = degrees_to_radians(angle);
-    sin_theta = sin(radians);
-    cos_theta = cos(radians);
-    hasbox = _object->bounding_box(0, 1, bbox);
-
-    Point3 min(infinity, infinity, infinity);
-    Point3 max(-infinity, -infinity, -infinity);
-
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            for (int k = 0; k < 2; ++k) {
-                auto x = i * bbox.max().x() + (1 - i) * bbox.min().x();
-                auto y = j * bbox.max().y() + (1 - j) * bbox.min().y();
-                auto z = k * bbox.max().z() + (1 - k) * bbox.min().z();
-
-                Vec3 tester = rotateY(Vec3(x, y, z));
-                for (int c = 0; c < 3; ++c) {
-                    min[c] = fmin(min[c], tester[c]);
-                    max[c] = fmax(max[c], tester[c]);
-                }
-            }
-        }
-    }
-    bbox = AABB(min, max);
-}
-
-bool Rotate_y::hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const {
-    auto origin = inverse_rotateY(ray.origin());
-    auto direction = inverse_rotateY(ray.direction());
-
-    Ray rotated_ray(origin, direction, ray.time());
-
-    if (!object->hit(rotated_ray, t_min, t_max, rec)) { return false; }
-
-    auto point = rotateY(rec.point);
-    auto normal = rotateY(rec.normal);
-
-    rec.point = point;
-    rec.set_face_normal(rotated_ray, normal);
-
-    return true;
+Rotate_y::Rotate_y(const shared_ptr<Hittable> &_object, double angle) : Rotate(_object, angle) {
+    compute_AABB();
 }
 
 
-class Rotate_x : public Hittable {
+class Rotate_x : public Rotate {
 public:
-    shared_ptr<Hittable> object;
-    double sin_theta;
-    double cos_theta;
-    bool hasbox;
-    AABB bbox;
 
     Rotate_x(const shared_ptr<Hittable> &_object, double angle);
 
-    bool hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const override;
-
-    bool bounding_box(double time0, double time1, AABB &output_box) const override {
-        output_box = bbox;
-        return hasbox;
-    }
-
 private:
 
-    [[nodiscard]] inline Vec3 rotateX(const Vec3 &vec) const {
+    [[nodiscard]] inline Vec3 rotate(const Vec3 &vec) const override {
         double new_y = cos_theta * vec.y() - sin_theta * vec.z();
         double new_z = sin_theta * vec.y() + cos_theta * vec.z();
 
         return {vec.x(), new_y, new_z};
     }
 
-    [[nodiscard]] inline Vec3 inverse_rotateX(const Vec3 &vec) const {
+    [[nodiscard]] inline Vec3 inverse_rotate(const Vec3 &vec) const override {
         double new_y = cos_theta * vec.y() + sin_theta * vec.z();
         double new_z = -sin_theta * vec.y() + cos_theta * vec.z();
 
@@ -174,78 +182,24 @@ private:
     }
 };
 
-Rotate_x::Rotate_x(const shared_ptr<Hittable> &_object, double angle) : object(_object) {
-    auto radians = degrees_to_radians(angle);
-    sin_theta = sin(radians);
-    cos_theta = cos(radians);
-    hasbox = _object->bounding_box(0, 1, bbox);
-
-    Point3 min(infinity, infinity, infinity);
-    Point3 max(-infinity, -infinity, -infinity);
-
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            for (int k = 0; k < 2; ++k) {
-                auto x = i * bbox.max().x() + (1 - i) * bbox.min().x();
-                auto y = j * bbox.max().y() + (1 - j) * bbox.min().y();
-                auto z = k * bbox.max().z() + (1 - k) * bbox.min().z();
-
-                Vec3 tester = rotateX(Vec3(x, y, z));
-
-                for (int c = 0; c < 3; ++c) {
-                    min[c] = fmin(min[c], tester[c]);
-                    max[c] = fmax(max[c], tester[c]);
-                }
-            }
-        }
-    }
-    bbox = AABB(min, max);
+Rotate_x::Rotate_x(const shared_ptr<Hittable> &_object, double angle) : Rotate(_object, angle) {
+    compute_AABB();
 }
 
-bool Rotate_x::hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const {
-    auto origin = inverse_rotateX(ray.origin());
-    auto direction = inverse_rotateX(ray.direction());
-
-    Ray rotated_ray(origin, direction, ray.time());
-
-    if (!object->hit(rotated_ray, t_min, t_max, rec)) { return false; }
-
-    auto point = rotateX(rec.point);
-    auto normal = rotateX(rec.normal);
-
-    rec.point = point;
-    rec.set_face_normal(rotated_ray, normal);
-
-    return true;
-}
-
-class Rotate_z : public Hittable {
+class Rotate_z : public Rotate {
 public:
-    shared_ptr<Hittable> object;
-    double sin_theta;
-    double cos_theta;
-    bool hasbox;
-    AABB bbox;
-
     Rotate_z(const shared_ptr<Hittable> &_object, double angle);
-
-    bool hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const override;
-
-    bool bounding_box(double time0, double time1, AABB &output_box) const override {
-        output_box = bbox;
-        return hasbox;
-    }
 
 private:
 
-    [[nodiscard]] inline Vec3 rotateZ(const Vec3 &vec) const {
+    [[nodiscard]] inline Vec3 rotate(const Vec3 &vec) const override {
         double new_x = cos_theta * vec.x() - sin_theta * vec.y();
         double new_y = sin_theta * vec.x() + cos_theta * vec.y();
 
         return {new_x, new_y, vec.z()};
     }
 
-    [[nodiscard]] inline Vec3 inverse_rotateZ(const Vec3 &vec) const {
+    [[nodiscard]] inline Vec3 inverse_rotate(const Vec3 &vec) const override {
         double new_x = cos_theta * vec.x() + sin_theta * vec.y();
         double new_y = -sin_theta * vec.x() + cos_theta * vec.y();
 
@@ -253,49 +207,8 @@ private:
     }
 };
 
-Rotate_z::Rotate_z(const shared_ptr<Hittable> &_object, double angle) : object(_object) {
-    auto radians = degrees_to_radians(angle);
-    sin_theta = sin(radians);
-    cos_theta = cos(radians);
-    hasbox = _object->bounding_box(0, 1, bbox);
-
-    Point3 min(infinity, infinity, infinity);
-    Point3 max(-infinity, -infinity, -infinity);
-
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            for (int k = 0; k < 2; ++k) {
-                auto x = i * bbox.max().x() + (1 - i) * bbox.min().x();
-                auto y = j * bbox.max().y() + (1 - j) * bbox.min().y();
-                auto z = k * bbox.max().z() + (1 - k) * bbox.min().z();
-
-                Vec3 tester = rotateZ(Vec3(x, y, z));
-
-                for (int c = 0; c < 3; ++c) {
-                    min[c] = fmin(min[c], tester[c]);
-                    max[c] = fmax(max[c], tester[c]);
-                }
-            }
-        }
-    }
-    bbox = AABB(min, max);
-}
-
-bool Rotate_z::hit(const Ray &ray, double t_min, double t_max, Hit_record &rec) const {
-    auto origin = inverse_rotateZ(ray.origin());
-    auto direction = inverse_rotateZ(ray.direction());
-
-    Ray rotated_ray(origin, direction, ray.time());
-
-    if (!object->hit(rotated_ray, t_min, t_max, rec)) { return false; }
-
-    auto point = rotateZ(rec.point);
-    auto normal = rotateZ(rec.normal);
-
-    rec.point = point;
-    rec.set_face_normal(rotated_ray, normal);
-
-    return true;
+Rotate_z::Rotate_z(const shared_ptr<Hittable> &_object, double angle) : Rotate(_object, angle) {
+    compute_AABB();
 }
 
 #endif //RAY_TRACING_IN_CPP_HITTABLE_H
